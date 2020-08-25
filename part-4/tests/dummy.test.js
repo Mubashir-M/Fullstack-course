433b8ca81c2a80b66c94a4040374a3fbef40d4ef
Mubashir-M <mubashir.mohamud@gmail.com>
2020-08-25 20:13:18 +0300

    completed 4.6

--- part-4/tests/dummy.test.js
+++ part-4/tests/dummy.test.js
@@ -1,5 +1,25 @@
 const listHelper = require('../utils/list_helper')
 
+const listWithOneBlog = [
+  {
+    _id: '5a422aa71b54a676234d17f8',
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
+    __v: 0
+  }
+]
+
+const listWithMultipleBlogs = [
+  { _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0 },
+  { _id: '5a422aa71b54a676234d17f8', title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5, __v: 0 },
+  { _id: '5a422b3a1b54a676234d17f9', title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', likes: 12, __v: 0 },
+  { _id: '5a422b891b54a676234d17fa', title: 'First class tests', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', likes: 10, __v: 0 },
+  { _id: '5a422ba71b54a676234d17fb', title: 'TDD harms architecture', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', likes: 0, __v: 0 },
+  { _id: '5a422bc61b54a676234d17fc', title: 'Type wars', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', likes: 22, __v: 0 }
+]
+
 test('dummy returns one', () => {
   const blogs = []
 
@@ -8,25 +28,6 @@ test('dummy returns one', () => {
 })
 
 describe('total likes', () => {
-  const listWithOneBlog = [
-    {
-      _id: '5a422aa71b54a676234d17f8',
-      title: 'Go To Statement Considered Harmful',
-      author: 'Edsger W. Dijkstra',
-      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
-      likes: 5,
-      __v: 0
-    }
-  ]
-
-  const listWithMultipleBlogs = [
-    { _id: '5a422a851b54a676234d17f7', title: 'React patterns', author: 'Michael Chan', url: 'https://reactpatterns.com/', likes: 7, __v: 0 },
-    { _id: '5a422aa71b54a676234d17f8', title: 'Go To Statement Considered Harmful', author: 'Edsger W. Dijkstra', url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html', likes: 5, __v: 0 },
-    { _id: '5a422b3a1b54a676234d17f9', title: 'Canonical string reduction', author: 'Edsger W. Dijkstra', url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html', likes: 12, __v: 0 },
-    { _id: '5a422b891b54a676234d17fa', title: 'First class tests', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll', likes: 10, __v: 0 },
-    { _id: '5a422ba71b54a676234d17fb', title: 'TDD harms architecture', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html', likes: 0, __v: 0 },
-    { _id: '5a422bc61b54a676234d17fc', title: 'Type wars', author: 'Robert C. Martin', url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html', likes: 2, __v: 0 }
-  ]
 
   test('when list has only one blog, equals the likes of that', () => {
     const result = listHelper.totalLikes(listWithOneBlog)
@@ -35,6 +36,36 @@ describe('total likes', () => {
 
   test('when list has multiple blogs, equals the sum of likes from all blogs', () => {
     const result = listHelper.totalLikes(listWithMultipleBlogs)
-    expect(result).toBe(36)
+    expect(result).toBe(56)
+  })
+})
+
+describe('favorite blog', () => {
+
+  test('when list has multiple blogs,equals the blog with highest likes', () => {
+    const result = listHelper.favoriteBlog(listWithMultipleBlogs)
+    const expected = {
+      title: listWithMultipleBlogs[5].title,
+      author: listWithMultipleBlogs[5].author,
+      likes: listWithMultipleBlogs[5].likes
+    }
+    console.log('here is result',result)
+    console.log('here is expected', expected)
+    expect(result).toEqual(expected)
+  })
+})
+
+describe ('most frequent author', () => {
+
+  test('when list has multiple blogs, equals the author with highest number of blogs', () => {
+    const result= listHelper.mostBlogs(listWithMultipleBlogs)
+    const expected = {
+      author:listWithMultipleBlogs[3].author,
+      blogs:3
+    }
+
+    console.log('here is result',result)
+    console.log('here is expected', expected)
+    expect(result).toEqual(expected)
   })
 })
--- part-4/utils/list_helper.js
+++ part-4/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
   // ...
   console.log('here is blog data:',blogs)
@@ -5,15 +7,69 @@ const dummy = (blogs) => {
 }
 
 const totalLikes = (blogs) => {
+  if (blogs.length===0){
+    return null
+  }
+
   const likes_sum = blogs.reduce((sum,blog) => {
     return sum+blog.likes
   },0)
   return likes_sum
 }
 
-// mostBlogs()
+const favoriteBlog= (blogs) => {
+  if (blogs.length===0){
+    return null
+  }
+
+  var result = blogs[0]
+  var i=0
+  for ( ; i< blogs.length; i++){
+    if (blogs[i].likes > result.likes) {
+      result = blogs[i]
+    }
+  }
+
+  result={
+    title: result.title,
+    author: result.author,
+    likes: result.likes
+  }
+  return result
+
+}
+
+const mostBlogs = (blogs) => {
+  if (blogs.length===0){
+    return null
+  }
+
+  var result= blogs[0]
+
+  const groupByAuthors=  _.sortBy(_.groupBy(blogs,'author'), (blog) =>  {
+    return blog.length
+  })
+
+
+  var count = 0
+  var i = 0
+
+  for (;i<groupByAuthors.length;i++){
+    if (groupByAuthors[i].length> count){
+      result = groupByAuthors[i][0]
+      count = groupByAuthors[i].length
+    }
+  }
+
+  result = {
+    author:result.author,
+    blogs:count
+  }
+  return result
+}
 // mostLikes()
+// groupby authors and map to (author,likeSum) using reduce for each group
 
 module.exports = {
-  dummy,totalLikes//mostBlogs,mostLikes
+  dummy,totalLikes,favoriteBlog,mostBlogs//mostLikes
 }
